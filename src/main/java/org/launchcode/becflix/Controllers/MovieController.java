9359c6e716002ebe1d95ce15f444b9d9caae9c64
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-19 23:36:42 -0500

    Added some java annotations to MovieController.

--- src/main/java/org/launchcode/becflix/Controllers/MovieController.java
+++ src/main/java/org/launchcode/becflix/Controllers/MovieController.java
@@ -1,4 +1,12 @@
 package org.launchcode.becflix.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("movie")
 public class MovieController {
+
+    
+
 }
