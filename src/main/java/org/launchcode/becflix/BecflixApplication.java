beed8212c7c0b1951795609cc55bfc8f1f125a56
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-19 21:51:42 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/becflix/BecflixApplication.java
@@ -0,0 +1,15 @@
+package org.launchcode.becflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class BecflixApplication {
+
+	public static void main(String[] args) {
+		run(BecflixApplication.class, args);
+	}
+
+}
