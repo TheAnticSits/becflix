57d5d691b9fbd0d5ece369cf238dfb3975bab30b
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-21 00:18:21 -0500

    Added structure to the classes. Getters and Setters in Movie class. Instance of the classes.

--- src/main/java/org/launchcode/becflix/Models/Movie.java
+++ src/main/java/org/launchcode/becflix/Models/Movie.java
@@ -7,4 +7,62 @@ public class Movie {
     private String director;
     private String genre;
     private int rating;
+    private String importance;
+
+    public Movie(){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+        this.rating = rating;
+        this.importance = importance;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getImportance() {
+        return importance;
+    }
+
+    public void setImportance(String importance) {
+        this.importance = importance;
+    }
 }
---
+++ src/main/java/org/launchcode/becflix/Models/Show.java
@@ -0,0 +1,19 @@
+package org.launchcode.becflix.Models;
+
+public class Show {
+    private String title;
+    private int year;
+    private String actor;
+    private String genre;
+    private String rating;
+    private String importance;
+
+    public Show(){
+        this.title = title;
+        this.year = year;
+        this.actor = actor;
+        this.genre = rating;
+        this.rating = rating;
+        this.importance = importance;
+    }
+}
