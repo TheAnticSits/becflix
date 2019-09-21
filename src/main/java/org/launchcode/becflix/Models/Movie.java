7b46c22749b876b957ac3b7719786ffca2c6bef5
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-21 00:00:34 -0500

    Added more to the Movie class.

---
+++ src/main/java/org/launchcode/becflix/Models/Movie.java
@@ -0,0 +1,10 @@
+package org.launchcode.becflix.Models;
+
+public class Movie {
+
+    private String title;
+    private int year;
+    private String director;
+    private String genre;
+    private int rating;
+}
--- src/main/java/org/launchcode/becflix/Models/data/Movie.java
+++
@@ -1,6 +0,0 @@
-package org.launchcode.becflix.Models.data;
-
-public class Movie {
-
-    private String title;
-}
