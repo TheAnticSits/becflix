28100083375f4ea8bffd5d6f9a4572fd2f9b8d7d
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-23 22:42:12 -0500

    finished some parts of moviecontroller and started my index html.

--- src/main/java/org/launchcode/becflix/Controllers/MovieController.java
+++ src/main/java/org/launchcode/becflix/Controllers/MovieController.java
@@ -2,11 +2,19 @@ package org.launchcode.becflix.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("movie")
+@Controller //spring recognizes it as a controller
 public class MovieController {
 
+    //method for the index
+    //value specifies the route to which requests should be mapped to this handler
+    //the ResponseBody annotation is used to bind the HTTP request/response body with a domain object in method parameter or return type
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index(){
 
+        return "The Start of the Movies.";
 
+    }
 }
