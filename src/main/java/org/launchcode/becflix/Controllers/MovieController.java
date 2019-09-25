9ef4e407f137c0c216f525314d63794063ea111f
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-24 21:52:47 -0500

    added requestmapping  movie to index. Refactored templates directory for templates that correspond to the movie controller. changed handler method to return movie/index

--- src/main/java/org/launchcode/becflix/Controllers/MovieController.java
+++ src/main/java/org/launchcode/becflix/Controllers/MovieController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //spring recognizes it as a controller
+@RequestMapping(value="movie") //establishes a base request path for every method within the controller.
 public class MovieController {
 
     //method for the index
     //value specifies the route to which requests should be mapped to this handler
     //the ResponseBody annotation is used to bind the HTTP request/response body with a domain object in method parameter or return type
     //@ResponseBody allows the controller method to return text directly from the controller method(the handler) removed from here.
+    //request path is now /movie
     @RequestMapping(value = "")
     public String index(){
 
-        return "index";
+        return "movie/index";
 
     }
 }
