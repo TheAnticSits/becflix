2fdaa7799a356d1d54873c4f37a3d0295a9a2b7b
Ken Messmer <kenmessmerjr@gmail.com>
2019-09-25 21:46:34 -0500

    Added a lot of notes. Added an arraylist of movies to the controller to check functonality. Added unordered list to the index to check functionality. Added a default display if there were no items in the object to be returned.

--- src/main/java/org/launchcode/becflix/Controllers/MovieController.java
+++ src/main/java/org/launchcode/becflix/Controllers/MovieController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 
 @Controller //spring recognizes it as a controller
 @RequestMapping(value="movie") //establishes a base request path for every method within the controller.
@@ -17,7 +19,18 @@ public class MovieController {
     //request path is now /movie
     @RequestMapping(value = "")
     public String index(Model model){
+
+        //creating an ArrayList called movies to pass into the view to test the view out.
+        ArrayList<String> movies = new ArrayList<>();
+        //adding a list of movies to the movies ArrayList
+        movies.add("Moving");
+        movies.add("Fletch");
+        movies.add("The Three Amigos");
+        //adding the object movies, the ArrayList, to the view using the name movies
+        model.addAttribute("movies", movies);
+
         //adding an attribute to the model in a key/value pair.
+        //the model is what passes data into the view.
         model.addAttribute("title", "My BecFlix");
         return "movie/index";
 
