83db88846f3f167b7bb5ded1d5b281722452d9b3
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-23 15:43:55 -0500

    Set all code to create a Show object

--- src/main/java/org/launchcode/becflix/Models/Show.java
+++ src/main/java/org/launchcode/becflix/Models/Show.java
@@ -1,21 +1,67 @@
 package org.launchcode.becflix.Models;
 
 public class Show {
-//    private int id;
+    private int showId;
     private String title;
     private int year;
-    private String actor;
+//    private String actor;
     private String genre;
     private String importance;
+    private static int nextId = 1;
 //    private Array keywords;
 //    private String spinoffOf;
 
 
-    public Show(){
+    public Show(String title, int year, String actor, String genre, String importance){
         this.title = title;
         this.year = year;
-        this.actor = actor;
+//        this.actor = actor;
         this.genre = genre;
+        this.importance = importance;
+    }
+
+    public Show() {
+        showId = nextId;
+        nextId++;
+    }
+
+    public int getShowId() {
+        return showId;
+    }
+
+    public void setShowId(int showId) {
+        this.showId = showId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImportance() {
+        return importance;
+    }
+
+    public void setImportance(String importance) {
         this.importance = importance;
     }
 }
