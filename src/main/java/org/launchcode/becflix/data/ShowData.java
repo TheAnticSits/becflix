d54805f1631f7a29ca0edd5baac2bef03f7d3c43
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-24 12:22:22 -0500

    added ShowData code for getAll getById add and remove. add is blocked for now because show has not been created yet.

--- src/main/java/org/launchcode/becflix/data/ShowData.java
+++ src/main/java/org/launchcode/becflix/data/ShowData.java
@@ -1,4 +1,29 @@
 package org.launchcode.becflix.data;
 
+import org.launchcode.becflix.models.Show;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShowData {
+
+    private static final Map<Integer, Show> shows = new HashMap<>();
+
+    public static Collection<Show> getAll(){
+        return shows.values();
+    }
+
+    public static Show getById(int id){
+        return shows.get(id);
+    }
+
+    /*private static void  add(Show show){
+        shows.put(show.getId(), show);
+    }*/
+
+    public static void remove(int id){
+        shows.remove(id);
+    }
 }
+
