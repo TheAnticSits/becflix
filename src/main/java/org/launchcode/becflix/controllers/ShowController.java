08e58fc4f0bac8bdb22141c47cecf7d4fbc4a11d
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-25 00:04:17 -0500

    Added Controller annotation to ShowController to render show index from /show

--- src/main/java/org/launchcode/becflix/controllers/ShowController.java
+++ src/main/java/org/launchcode/becflix/controllers/ShowController.java
@@ -1,14 +1,15 @@
 
 package org.launchcode.becflix.controllers;
 
-
 import org.launchcode.becflix.data.ShowData;
 import org.launchcode.becflix.models.Show;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 @RequestMapping(value="show")
 public class ShowController {
 
