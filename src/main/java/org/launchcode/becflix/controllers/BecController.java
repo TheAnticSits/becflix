44263ed2dd0ecf72adac48cce099b49d6642f325
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-26 16:22:55 -0500

    Deleted excess code from BecController

--- src/main/java/org/launchcode/becflix/controllers/BecController.java
+++ src/main/java/org/launchcode/becflix/controllers/BecController.java
@@ -23,45 +23,4 @@ public class BecController {
         return "movie/index";
     }
 
-    @RequestMapping(value="addMovie", method = RequestMethod.GET)
-    public String displayAddMovieForm(Model model){
-
-        model.addAttribute("title", "Add Movie");
-        return "movie/addMovie";
-    }
-
-    @RequestMapping(value="addMovie", method = RequestMethod.POST)
-    public String processAddMovieForm(@RequestParam String movieName, @RequestParam int year, @RequestParam String genre, @RequestParam String director, @RequestParam String franchise, @RequestParam String rating, @RequestParam String importance){
-        String noEntry = "No Entry";
-        String noInt = "";
-        if(director == null){
-            director = noEntry;
-        }
-        if(franchise.equals(null)) franchise = noEntry;
-        if(noInt.equals(year)){
-            year = 0;
-        }
-
-        Movie newMovie = new Movie(movieName, year, genre, director, franchise, rating, importance);
-
-        MovieData.add(newMovie);
-        return "redirect:";
-    }
-
-    @RequestMapping(value = "removeMovie", method = RequestMethod.GET)
-    public String displayRemoveMovieForm(Model model){
-        model.addAttribute("movies", MovieData.getAll());
-        model.addAttribute("title", "Remove Movie(s)");
-        return "movie/removeMovie";
-    }
-
-    @RequestMapping(value = "removeMovie", method = RequestMethod.POST)
-    public String processRemoveMovieForm(@RequestParam int[] movieIds){
-
-        for(int movieId : movieIds){
-
-            MovieData.remove(movieId);
-        }
-        return "redirect:";
-    }
 }
