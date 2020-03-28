44baf1aaae453b72d9cb82d1e129d3ee455e9b9f
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-28 00:32:52 -0500

    Added a requestmapping to the usercontroller for displaying the user form for adding a user

--- src/main/java/org/launchcode/becflix/controllers/UserController.java
+++ src/main/java/org/launchcode/becflix/controllers/UserController.java
@@ -1,9 +1,18 @@
 package org.launchcode.becflix.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value="addUser")
+    public String displayAddUserForm(Model model){
+
+        model.addAttribute("title", "Add Movie");
+        return "addUser";
+    }
+
 }
