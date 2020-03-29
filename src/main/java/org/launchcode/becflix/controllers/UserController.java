a870c9ba3a6109a615bf3c74ed2392dc75e649cf
Ken Messmer <kenmessmerjr@gmail.com>
2020-03-28 21:33:03 -0500

    working on UserController and data files for user

--- src/main/java/org/launchcode/becflix/controllers/UserController.java
+++ src/main/java/org/launchcode/becflix/controllers/UserController.java
@@ -2,17 +2,19 @@ package org.launchcode.becflix.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value="addUser")
-    public String displayAddUserForm(Model model){
 
-        model.addAttribute("title", "Add Movie");
-        return "addUser";
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("title", "BecFlix");
+        return "index";
     }
+
 
 }
