1d1a336b52552e02f274347017682c6e9ebe4cac
Ken Messmer <kenmessmerjr@gmail.com>
2020-04-03 20:57:05 -0500

    Added Annotation to User Model for validation.

--- src/main/java/org/launchcode/becflix/models/User.java
+++ src/main/java/org/launchcode/becflix/models/User.java
@@ -1,13 +1,28 @@
 package org.launchcode.becflix.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
 
     private int userId;
-    private String username;
-    private String email;
-    private String password;
-    private String verifyPass;
     private static int nextId = 1;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 5, max = 15, message = "Password Must Be Between 5 to 15 Characters.")
+    private String password;
+    
+    private String verifyPass;
+
 
     public User(String username, String email, String password, String verifyPass){
         this.username = username;
