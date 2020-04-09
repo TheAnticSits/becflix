4a23c02bf1ca50dd70f87677f70e14db48ffa055
Ken Messmer <kenmessmerjr@gmail.com>
2020-04-08 22:22:24 -0500

    Updated MovieData with Hashmap and Collection.

--- src/main/java/org/launchcode/becflix/data/MovieData.java
+++ src/main/java/org/launchcode/becflix/data/MovieData.java
@@ -2,22 +2,27 @@ package org.launchcode.becflix.data;
 
 import org.launchcode.becflix.models.Movie;
 
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MovieData {
 
-    static ArrayList<Movie> movies = new ArrayList<>();
+    private static final Map<Integer, Movie> movies = new HashMap<>();
 
-    //need to getAll
-    public static ArrayList<Movie> getAll() {
-        return movies;
+    public static Collection<Movie> getAll(){
+        return movies.values();
     }
 
+/*
     //need add
     public static void add(Movie newMovie) {
         movies.add(newMovie);
     }
     //need remove
+*/
 
     public static void remove(int id) {
         Movie movieToRemove = getById(id);
@@ -28,11 +33,11 @@ public class MovieData {
     public static Movie getById(int id) {
         Movie theMovie = null;
 
-        for (Movie candidateMovie : movies) {
+ /*       for (Movie candidateMovie : movies) {
             if (candidateMovie.getMovieId() == id) {
                 theMovie = candidateMovie;
             }
-        }
+        }*/
         return theMovie;
     }
 }
