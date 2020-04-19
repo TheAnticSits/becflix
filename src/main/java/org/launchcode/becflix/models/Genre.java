b9580e90ed0be91dd2ea0f4dd59d34b20eb4f3a5
Ken Messmer <kenmessmerjr@gmail.com>
2020-04-19 01:26:04 -0500

    Created an Enum for Genre

---
+++ src/main/java/org/launchcode/becflix/models/Genre.java
@@ -0,0 +1,60 @@
+package org.launchcode.becflix.models;
+
+public enum Genre {
+
+    UNKNOWN("I Don't Know"),
+    ACTION("Action"),
+    ADULT("Adult"),
+    ADVENTURE("Adventure"),
+    ANIME("Anime"),
+    BIOGRAPHICAL("Biographical"),
+    BUDDY("Buddy"),
+    CHICKFLICK("Chick Flick"),
+    COMEDY("Comedy"),
+    CONCERT("Concert"),
+    CRIME("Crime"),
+    DARKCOMEDY("Dark Comedy"),
+    DISASTER("Disaster"),
+    DOCUMENTARY("Documentary"),
+    DRAMA("Drama"),
+    EPIC("Epic"),
+    EXPLOITATION("Exploitation"),
+    FAMILY("Family"),
+    FANTASY("Fantasy"),
+    GANGSTER("Gangster"),
+    HEIST("Heist"),
+    HISTORICAL("Historical"),
+    HORROR("Horror"),
+    LEGALDRAMA("Legal Drama"),
+    MARTIALARTS("Martial Arts"),
+    MOCKUMENTARY("Mockumentary"),
+    MUSICAL("Musical"),
+    MYSTERY("Mystery"),
+    NOIR("Noir"),
+    PARODY("Parody"),
+    PSYCHOLOGICALTHRILLER("Psychological Thriller"),
+    ROMANCE("Romance"),
+    ROMCOM("Romantic Comedy"),
+    SCIFI("Science Fiction"),
+    SILENTFILM("Silent Film"),
+    SLASHER("Slasher"),
+    SPORTS("Sports"),
+    SPY("Spy"),
+    SUPERHERO("Superhero"),
+    SWASHBUCKLER("Swashbuckler"),
+    TEEN("Teen"),
+    THRILLER("Thriller"),
+    WAR("War"),
+    WESTERN("Western");
+
+    private final String genre;
+
+    Genre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
+
