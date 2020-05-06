2f1edf78e4e6552756add30cee0aadad2c4281fd
Ken Messmer <kenmessmerjr@gmail.com>
2020-05-06 17:12:45 -0500

    Set up releaseDate object with constructor and getters and setters

--- src/main/java/org/launchcode/becflix/models/ReleaseDate.java
+++ src/main/java/org/launchcode/becflix/models/ReleaseDate.java
@@ -1,9 +1,22 @@
 package org.launchcode.becflix.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class ReleaseDate {
 
     private Date releaseDate;
 
+    public ReleaseDate(Date releaseDate){
+        this.releaseDate = releaseDate;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
