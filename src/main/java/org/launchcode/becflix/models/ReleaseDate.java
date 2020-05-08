7534881d89d355fd82136991024675ff4311da78
Ken Messmer <kenmessmerjr@gmail.com>
2020-05-07 21:47:48 -0500

    going to try another route with date

--- src/main/java/org/launchcode/becflix/models/ReleaseDate.java
+++ src/main/java/org/launchcode/becflix/models/ReleaseDate.java
@@ -1,25 +1,8 @@
 package org.launchcode.becflix.models;
 
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.text.DateFormat;
 
 public class ReleaseDate {
 
-    @DateTimeFormat(style = "MM/dd/yyyy")
-    private DateFormat releaseDate;
 
-    public ReleaseDate(DateFormat releaseDate){
-        this.releaseDate = releaseDate;
-        System.out.println(releaseDate);
-        System.out.println("Trial");
-    }
-
-    public DateFormat getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(DateFormat releaseDate) {
-        this.releaseDate = releaseDate;
-    }
 }
