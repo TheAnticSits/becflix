1f8462d88594eab19f10e2c13c74e3db062c67ff
Ken Messmer <kenmessmerjr@gmail.com>
2020-05-12 22:30:40 -0500

    Created a Month enum for date purposes.

---
+++ src/main/java/org/launchcode/becflix/models/Month.java
@@ -0,0 +1,28 @@
+package org.launchcode.becflix.models;
+
+public enum Month {
+
+    JANUARY("JAN"),
+    FEBRUARY("FEB"),
+    MARCH("MAR"),
+    APRIL("APR"),
+    MAY("MAY"),
+    JUNE("JUN"),
+    JULY("JUL"),
+    AUGUST("AUG"),
+    SEPTEMBER("SEP"),
+    OCTOBER("OCT"),
+    NOVEMBER("NOV"),
+    DECEMBER("DEC");
+
+    private final String displayMonth;
+
+    Month(String displayMonth) {
+        this.displayMonth = displayMonth;
+    }
+
+    public String getDisplayMonth() {
+        return displayMonth;
+    }
+
+}
