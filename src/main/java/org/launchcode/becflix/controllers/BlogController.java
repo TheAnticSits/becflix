429aa7df102a114c796193f2c8b5210dc6019588
Ken Messmer <kenmessmerjr@gmail.com>
2020-06-14 23:45:04 -0500

    Added Blog Contoller.  Added some code refactored from UserController to BlogController.  Need to add quite a bit to make the Blog function.

---
+++ src/main/java/org/launchcode/becflix/controllers/BlogController.java
@@ -0,0 +1,59 @@
+package org.launchcode.becflix.controllers;
+
+
+import org.launchcode.becflix.data.UserData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("blog")
+public class BlogController {
+
+/*
+
+
+    @GetMapping
+    public String displayNewUser(Model model) {
+        model.addAttribute("blogs", BlogData.getAll());
+        model.addAttribute("title", "Blogs");
+        return "user/index";
+    }
+
+
+    @GetMapping("addUser")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title", "Add User");
+        model.addAttribute(new Blog());
+        model.addAttribute("genres", GenreType.values());
+        return "user/addUser";
+    }
+
+    @PostMapping(value = "addUser")
+    public String processAddUserForm(@ModelAttribute @Valid Blog newUser, Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "New User");
+
+            return "user/addUser";
+        } else {
+            if (newUser.getPassword().equals((newUser.getVerifyPass()))){
+                UserData.add(newUser);
+                return "redirect:";
+            }
+            else{
+                return "user/addUser";
+            }
+
+        }
+    }
+*/
+
+
+
+}
