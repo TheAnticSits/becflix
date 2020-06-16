f76f7cc224afaa648f7e13ade93bf069fa43f846
Ken Messmer <kenmessmerjr@gmail.com>
2020-06-16 16:08:05 -0500

    Created A BlogData Class and refactored from movies.

---
+++ src/main/java/org/launchcode/becflix/data/BlogData.java
@@ -0,0 +1,25 @@
+package org.launchcode.becflix.data;
+
+public class BlogData {
+/*
+    private static final Map<Integer, Blog> movies = new HashMap<>();
+
+    public static Collection<Blog> getAll(){
+        return blogs.values();
+    }
+
+    public static Blog getById(int id){
+        return blogs.get(id);
+    }
+
+    public static void  add(Blog movie){
+
+        blogs.put(movie.getBlogId(), blog);
+    }
+
+    public static void remove(int id){
+
+        blogs.remove(id);
+    }
+    */
+}
